ae9a0fb043860e6a1e696a0b23f0a161b362268e
Roberto Batty <texas_telecaster@hotmail.com>
2020-11-22 23:08:54 -0500

    improving ci

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,19 @@ async function run(): Promise<void> {
     const path : any = core.getInput('path') || process.env.GITHUB_WORKSPACE;
     const npm_package_name = core.getInput('npm_package_name')
 
-    core.debug(`Load package.json at ${path}`)
+    core.info(`Load package.json at ${path}`)
 
     const local_version = getNodeVersion(path)
 
-    core.debug(`set output: local version: ${local_version}`)
+    core.info(`set output: local version: ${local_version}`)
     core.setOutput('version', local_version)
 
     const { npm_is_greater, pkg_npm_version } = await compareTheseVersions(npm_package_name, local_version)
 
-    core.debug(`set output: npm version: ${pkg_npm_version}`)
+    core.info(`set output: npm version: ${pkg_npm_version}`)
     core.setOutput('pkg_npm_version', `${pkg_npm_version}`)
 
-    core.debug(`set output: Is npm version greater than local?: ${npm_is_greater}`)
+    core.info(`set output: Is npm version greater than local?: ${npm_is_greater}`)
     core.setOutput('npm_is_greater', `${npm_is_greater}`)
 
 
